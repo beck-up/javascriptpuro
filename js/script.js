d138bac86a30f1212edfc659262b7681ee5f0c03
Bequem <1thebeck@gmail.com>
2021-04-20 03:01:07 -0300

    mini loja em JS POO

---
+++ js/script.js
@@ -0,0 +1,19 @@
+class Produto {
+
+    constructor() {
+        this.id = 0;
+        this.nameProduto = '';
+        this.valor = 0;
+        
+    }
+
+    adicionar() {
+        alert('Vamos adicionar um produto');
+    }
+
+    excluir() {
+        alert('Item excluido');
+    }
+}
+
+var produto = new Produto();
